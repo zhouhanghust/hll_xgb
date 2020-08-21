b99ac159780c0501ef0e04082a1a2ef08a0fc3d5
nicolas.zhou <zhouhanghust@126.com>
2020-08-21 13:39:47 +0800

    2020-08-21

--- dataAnalyzer/utils/SparkEnv.py
+++ dataAnalyzer/utils/SparkEnv.py
@@ -20,11 +20,21 @@ class SparkEnv():
             .config('spark.dynamicAllocation.minExecutors', 16) \
             .config('spark.dynamicAllocation.maxExecutors', 16) \
             .config('spark.executor.instances', 16) \
+            .config('spark.hadoop.hive.exec.dynamic.partition', True) \
+            .config('spark.hadoop.hive.exec.dynamic.partition.mode', 'nonstrict') \
+            .config('spark.sql.sources.partitionOverwriteMode', 'dynamic') \
+            .config('spark.jars', 'hdfs:///user/yuan.shi/spark/jar/xgboost4j-0.90.jar,hdfs:///user/yuan.shi/spark/jar/xgboost4j-spark-0.90.jar') \
             .enableHiveSupport() \
             .getOrCreate()
 
+    # .config('spark.yarn.dist.archives' ,'hdfs:///user/yuan.shi/spark/py/py_env.tar.gz#py_env') \
+    # .config('spark.pyspark.driver.python', './py_env/bin/python') \
+    # .config('spark.pyspark.python', './py_env/bin/python') \
+
+
     @classmethod
     def getSession(cls):
+        cls._spark.sparkContext.addPyFile('hdfs:///user/yuan.shi/spark/py/sparkxgb.zip')
         return cls._spark
 
 
