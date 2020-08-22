bc4c7702aa1181ee0ac8ac3eaaa8f034fe622068
nicolas.zhou <zhouhanghust@126.com>
2020-08-22 18:21:46 +0800

    last commit bf pending

--- XGBoostCVTestData.py
+++ XGBoostCVTestData.py
@@ -1,1 +1,54 @@
 # encoding=utf-8
+import numpy as np
+import pandas as pd
+import json, time, datetime
+import os, sys
+from utils.Hdfs2Df import Hdfs2Df
+
+
+os.environ['PYSPARK_PYTHON'] = '/data/zhouhang/py_env/bin/python3'
+os.environ['PYSPARK_DRIVER_PYTHON'] = '/data/zhouhang/py_env/bin/python3'
+os.environ['SPARK_HOME'] = '/usr/hdp/current/spark2-client'
+
+# 添加pyspark包路径
+sys.path.append('/usr/hdp/current/spark2-client/python')
+sys.path.append('/usr/hdp/current/spark2-client/python/lib/py4j-0.10.7-src.zip')
+# 添加sparkxgb包路径
+sys.path.append('/data/zhouhang/sparkxgb_all')
+sys.path.append('/data/zhouhang/sparkxgb_all/xgboost4j-spark-0.72.jar')
+sys.path.append('/data/zhouhang/sparkxgb_all/xgboost4j-0.72.jar')
+
+
+from dataAnalyzer.utils.SparkEnv import SparkEnv
+from params.params import config
+from preProcessor.PreProcessor import PreProcessor
+from utils.SavaTools import SavaTools
+from classification.XGBoostClassifier import XGBoostClassifier
+from evaluation.Evaluator import Evaluator
+from evaluation.CrossValidationKs import CrossValidationKs
+
+
+def getCVResult(spark):
+    train_df = Hdfs2Df.readHdfsCsv(spark=spark, data_path=config['TRAIN']['train_path'])
+    test_df = Hdfs2Df.readHdfsCsv(spark=spark, data_path=config['TRAIN']['test_path'])
+
+    missing = config['XGBOOST']['missing']
+    train_df = PreProcessor.transColType(train_df, missing)
+    test_df = PreProcessor.transColType(test_df, missing)
+    train, train_col = PreProcessor.transVector(train_df, 'features')
+    test, test_col = PreProcessor.transVector(train_df,'features')
+
+    xgb_cv = CrossValidationKs(spark,test,config['TRAIN']['cv_num'])
+    xgb_cv.crossValidationByXGB(config['XGBOOST'])
+
+
+if __name__ == "__main__":
+    spark = SparkEnv.getSession()
+    getCVResult(spark)
+
+
+
+
+
+
+
---
+++ evaluation/CrossValidationKs.py
@@ -0,0 +1,69 @@
+# encoding=utf-8
+from classification.XGBoostClassifier import XGBoostClassifier
+from evaluation.Evaluator import Evaluator
+from params.params import config
+import numpy as np
+
+
+class CrossValidationKs():
+    def __init__(self, spark, train, cv_num):
+        self.spark = spark
+        self.train = train
+        self.cv_num = cv_num
+
+    def crossValidationByXGB(self, paramMap):
+        split_num_arr = []
+        for i in range(self.cv_num):
+            split_num_arr.append(1.0 / self.cv_num)
+        split_data_arr = self.train.randomSplit(split_num_arr, seed=666)
+        split_data_arr.map(lambda x: x.cache())
+        kfold_res = self.KFold(split_data_arr)
+        train_ks_list = []
+        val_ks_list = []
+        train_auc_list = []
+        val_auc_list = []
+
+        for i in range(len(kfold_res)):
+            train_df, validation_df = kfold_res[i]
+            xgb_handle = XGBoostClassifier(config['XGBOOST'])
+            xgbModel = xgb_handle.train(train_df)
+            train_res, train_auc = xgb_handle.predict(self.spark, train_df, xgbModel)
+            validation_res, validation_auc = xgb_handle.predict(self.spark, validation_df, xgbModel)
+
+            evaluator_handler = Evaluator(self.spark)
+            train_ks = evaluator_handler.evaluateKsCustomized(train_res, "score")
+            val_ks = evaluator_handler.evaluateKsCustomized(validation_res,"score")
+            print("fold"+str(i)+" train ks: {0}".format(train_ks))
+            print("fold" + str(i) + " val ks: {0}".format(val_ks))
+            print("fold" + str(i) + " train auc: {0}".format(train_auc))
+            print("fold" + str(i) + " val auc: {0}".format(validation_auc))
+
+            train_ks_list.append(train_ks)
+            train_auc_list.append(train_auc)
+            val_ks_list.append(val_ks)
+            val_auc_list.append(validation_auc)
+
+        print('----------------avg----------------')
+        print("train average ks is ", np.mean(train_ks_list))
+        print("val average ks is ", np.mean(val_ks_list))
+        print("train average auc is ", np.mean(train_auc_list))
+        print("val average auc is ", np.mean(val_auc_list))
+
+
+    def KFold(self, arr_df):
+        train_df = self.spark.emptyDataFrame
+        validation_df = self.spark.emptyDataFrame
+        kfold_res = []
+        for i in range(len(arr_df)):
+            validation_df = arr_df[i]
+            flag = True
+            for j in range(len(arr_df)):
+                if flag and j!=i:
+                    train_df = arr_df[j]
+                elif not flag and j!=i:
+                    train_df = train_df.union(arr_df[j])
+                if j!=i:
+                    flag = True
+            kfold_res.append([train_df, validation_df])
+
+        return kfold_res
